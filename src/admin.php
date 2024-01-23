d8ebd49d503741805b49a53dda6776c528fc7672
Ludiphil <jocelyn.grelier@reseau.eseo.fr>
2024-01-23 16:25:45 +0100

    test

--- src/admin.php
+++ src/admin.php
@@ -11,4 +11,21 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
 
 // if the user is logged in and is an admin, display the admin page
 include 'administrateur.html';
+
+include 'db_connect.php';
+
+// Perform a query
+$sql = "SELECT * FROM Dessin";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // Output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["numDessin"];
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
 ?>

0542e6bac6c9c27fc0f16778fd1d0053285f7b3b
Ludiphil <jocelyn.grelier@reseau.eseo.fr>
2024-01-23 16:45:36 +0100

    test

--- src/admin.php
+++ src/admin.php
@@ -9,32 +9,6 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
     exit();
 }
 
-include 'db_connect.php';
-
-echo "Included db_connect.php.<br>";
-
-// Perform a query
-$sql = "SELECT * FROM Dessin";
-$result = $conn->query($sql);
-
-if ($result === FALSE) {
-    die("SQL Error: " . $conn->error);
-}
-
-echo "Performed SQL query.<br>";
-
-echo "<div style='background-color: yellow;'>";
-if ($result->num_rows > 0) {
-  // Output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<p>ID du dessin : " . $row["numDessin"] . "</p>";
-  }
-} else {
-  echo "0 results";
-}
-echo "</div>";
-$conn->close();
-
 echo "User is an admin.<br>";
 
 // if the user is logged in and is an admin, display the admin page
--- src/navbar.php
+++ src/navbar.php
@@ -2,6 +2,38 @@
 // start the session
 session_start();
 var_dump($_SESSION);
+
+// Include the database connection file
+include 'db_connect.php';
+
+// Test the database connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully.<br>";
+}
+
+// Perform a query
+$sql = "SELECT * FROM Dessin";
+$result = $conn->query($sql);
+
+if ($result === FALSE) {
+    die("SQL Error: " . $conn->error);
+}
+
+echo "Performed SQL query.<br>";
+
+echo "<div style='background-color: yellow;'>";
+if ($result->num_rows > 0) {
+  // Output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<p>ID du dessin : " . $row["numDessin"] . "</p>";
+  }
+} else {
+  echo "0 results";
+}
+echo "</div>";
+$conn->close();
 ?>
 
 <!DOCTYPE html>

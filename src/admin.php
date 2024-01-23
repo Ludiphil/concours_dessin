b878c99154b95b23f4b6a84bb943e071d8535a7b
Ludiphil <jocelyn.grelier@reseau.eseo.fr>
2024-01-23 11:07:13 +0100

    test

--- src/admin.php
+++ src/admin.php
@@ -2,13 +2,13 @@
 // start the session
 session_start();
 
-// check if the user is an admin
-if ($_SESSION['role'] != 'admin') {
-    // if not, redirect to the login page
-    header('Location: connexion.html');
+// check if the user is logged in and is an admin
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
+    // if not, redirect them to the home page
+    header('Location: index.html');
     exit();
 }
 
-// if the user is an admin, include the admin HTML file
+// if the user is logged in and is an admin, display the admin page
 include 'administrateur.html';
 ?>

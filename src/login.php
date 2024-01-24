2bdd610c96c8f3f0e382a326654f878f5d40ddaf
Ludiphil <jocelyn.grelier@reseau.eseo.fr>
2024-01-24 11:43:28 +0100

    Qu'est ce qui n'est pas un steak ? Une pastèque

--- src/login.php
+++ src/login.php
@@ -6,27 +6,6 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// check the credentials (this is just a placeholder, you should check the credentials against a database)
-if ($username == 'admin' && $password == 'password') {
-    $_SESSION['role'] = 'admin';
-} elseif ($username == 'competitor' && $password == 'password') {
-    $_SESSION['role'] = 'competitor';
-} elseif ($username == 'evaluator' && $password == 'password') {
-    $_SESSION['role'] = 'evaluator';
-} else {
-    // if the credentials are wrong, redirect back to the login page with an error message
-    header('Location: connexion.html?error=1');
-    exit();
-}
-
-// if the credentials are correct, redirect to the correct page based on the role
-if ($_SESSION['role'] == 'admin') {
-    header('Location: admin.php');
-} elseif ($_SESSION['role'] == 'competitor') {
-    header('Location: competitor.php');
-} elseif ($_SESSION['role'] == 'evaluator') {
-    header('Location: evaluator.php');
-}
 
 // Create the SQL query
 $sql = "SELECT * FROM Utilisateur WHERE login = ?";
